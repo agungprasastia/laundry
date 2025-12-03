8fb05dfa90ca4795b24d8182a58f32cd4fc90230
Agung Prasasti Abadi <agungprasastia@gmail.com>
2025-12-03 09:14:21 +0800

    initial commit

--- config/cek_login.php
+++ config/cek_login.php
@@ -19,26 +19,22 @@ if($cek_admin > 0){
     header("location: ../pages/admin/dashboard.php"); 
 
 } else {
-    // --- TAHAP 2: CEK TABEL PELANGGAN ---
     $login_user = mysqli_query($conn, "SELECT * FROM pelanggan WHERE username='$username' AND password='$password'");
     $cek_user = mysqli_num_rows($login_user);
     
     if($cek_user > 0){
-        // LOGIN SEBAGAI PELANGGAN
         $data = mysqli_fetch_array($login_user);
         
         $_SESSION['username'] = $username;
         $_SESSION['nama_pelanggan'] = $data['nama_lengkap'];
         $_SESSION['id_pelanggan'] = $data['id_pelanggan'];
         
-        // PERBAIKAN: Gunakan nama 'status' dan nilai 'login'
         $_SESSION['status'] = "login"; 
         $_SESSION['role'] = "pelanggan";
         
     header("location: ../pages/user/user_dashboard.php"); 
         
     } else {
-        // --- GAGAL KEDUANYA ---
         header("location: ../login.php?pesan=gagal");
     }
 }
